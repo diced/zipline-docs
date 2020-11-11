b186bdb20ffeaa7d2d8de813cb794ce7e788f6e7
diced <pranaco2@gmail.com>
2020-11-11 09:18:51 -0800

    op

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,76 +1,82 @@
 module.exports = {
-  title: 'Zipline',
-  tagline: 'Fast & lightweight file uploading.',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  favicon: 'img/favicon.ico',
-  organizationName: 'dicedtomatoreal', // Usually your GitHub org/user name.
-  projectName: 'zipline', // Usually your repo name.
+  title: "Zipline",
+  tagline: "Fast & lightweight file uploading.",
+  url: "https://zipline.diced.wtf/",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  favicon: "img/favicon.ico",
+  organizationName: "diced", // Usually your GitHub org/user name.
+  projectName: "zipline", // Usually your repo name.
   themeConfig: {
     algolia: {
-      apiKey: '6c53c673cd48e2713f933f9f398412a0',
-      indexName: 'zipline',
+      apiKey: "6c53c673cd48e2713f933f9f398412a0",
+      indexName: "zipline",
       contextualSearch: true,
       searchParameters: {},
     },
     announcementBar: {
-      id: 'starzipline',
+      id: "starzipline",
       content:
         '⭐️ If you like Zipline, give it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/dicedtomatoreal/zipline">GitHub</a>! ⭐️',
     },
     prism: {
-      theme: require('prism-react-renderer/themes/github'),
-      darkTheme: require('prism-react-renderer/themes/dracula'),
+      theme: require("prism-react-renderer/themes/github"),
+      darkTheme: require("prism-react-renderer/themes/dracula"),
     },
     navbar: {
-      title: 'Zipline',
+      title: "Zipline",
       logo: {
-        alt: 'Zipline Zip Logo',
-        src: 'img/logo.svg',
+        alt: "Zipline Zip Logo",
+        src: "img/logo.svg",
       },
       items: [
         {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
+          to: "docs/",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "left",
         },
         {
-          href: 'https://github.com/dicedtomatoreal/zipline',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/diced/zipline",
+          label: "GitHub",
+          position: "right",
         },
         {
-          href: 'https://discord.gg/PWU8rxy',
-          label: 'Discord',
-          position: 'right',
+          href: "https://github.com/diced/zipline-docs",
+          label: "GitHub Docs",
+          position: "right",
+        },
+        {
+          href: "https://discord.gg/PWU8rxy",
+          label: "Join us on Discord",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       logo: {
-        alt: 'Deploy with Vercel',
-        src: 'https://vercel.com/button',
-        href: 'https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Fdicedtomatoreal%2Fzipline-docs%2Ftree%2Fmaster'
+        alt: "Deploy with Vercel",
+        src: "https://vercel.com/button",
+        href:
+          "https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Fdicedtomatoreal%2Fzipline-docs%2Ftree%2Fmaster",
       },
       copyright: `Copyright © ${new Date().getFullYear()} ZiplineProject. Built with Docusaurus. `,
     },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           editUrl:
-            'https://github.com/dicedtomatoreal/zipline-docs/edit/master/',
+            "https://github.com/dicedtomatoreal/zipline-docs/edit/master/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        }
-      }
-    ]
-  ]
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
 };
