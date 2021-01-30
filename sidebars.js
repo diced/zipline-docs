f5d7ee1850d80297886783fcaa1fdedee4e58aa8
diced <pranaco2@gmail.com>
2021-01-30 11:48:10 -0800

    feat(docs/uploaders): add flameshot examples

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   someSidebar: {
     Zipline: ['getting-started', 'docker', 'auto-setup', 'migrations', 'faq', 'NGINX'],
     Configuration: ['configuration', 'config/example', 'config/meta', 'config/core', 'config/uploader', 'config/urls', 'config/webhooks'],
-    Uploaders: ['uploaders/sharex', 'uploaders/curl'],
+    Uploaders: ['uploaders/sharex', 'uploaders/curl', 'uploaders/flameshot'],
     Plugins: ['plugins/getting-started']
   },
 };
