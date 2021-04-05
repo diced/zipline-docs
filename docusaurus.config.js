dcf33cd9b4dcc8b98fefe422e359d1991d09d795
diced <pranaco2@gmail.com>
2021-04-05 14:19:49 -0700

    set default to dark theme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,50 +8,53 @@ module.exports = {
   organizationName: "diced", // Usually your GitHub org/user name.
   projectName: "zipline", // Usually your repo name.
   themeConfig: {
+    colorMode: {
+      defaultMode: "dark"
+    },
     algolia: {
       apiKey: "6c53c673cd48e2713f933f9f398412a0",
       indexName: "zipline",
       contextualSearch: true,
-      searchParameters: {},
+      searchParameters: {}
     },
     announcementBar: {
       id: "starzipline",
       content:
-        '⭐️ If you like Zipline, give it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/dicedtomatoreal/zipline">GitHub</a>! ⭐️',
+        '⭐️ If you like Zipline, give it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/dicedtomatoreal/zipline">GitHub</a>! ⭐️'
     },
     prism: {
       theme: require("prism-react-renderer/themes/github"),
-      darkTheme: require("prism-react-renderer/themes/dracula"),
+      darkTheme: require("prism-react-renderer/themes/dracula")
     },
     navbar: {
       title: "Zipline",
       logo: {
         alt: "Zipline Logo",
-        src: "img/logo.svg",
+        src: "img/logo.svg"
       },
       items: [
         {
           to: "docs/",
           activeBasePath: "docs",
           label: "Docs",
-          position: "left",
+          position: "left"
         },
         {
           href: "https://github.com/diced/zipline",
           label: "GitHub",
-          position: "right",
+          position: "right"
         },
         {
           href: "https://github.com/diced/zipline-docs",
           label: "GitHub Docs",
-          position: "right",
+          position: "right"
         },
         {
           href: "https://discord.gg/PWU8rxy",
           label: "Join us on Discord",
-          position: "right",
-        },
-      ],
+          position: "right"
+        }
+      ]
     },
     footer: {
       style: "dark",
@@ -59,10 +62,10 @@ module.exports = {
         alt: "Deploy with Vercel",
         src: "https://vercel.com/button",
         href:
-          "https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Fdicedtomatoreal%2Fzipline-docs%2Ftree%2Fmaster",
+          "https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Fdicedtomatoreal%2Fzipline-docs%2Ftree%2Fmaster"
       },
-      copyright: `Copyright © ${new Date().getFullYear()} ZiplineProject. Built with Docusaurus. `,
-    },
+      copyright: `Copyright © ${new Date().getFullYear()} ZiplineProject. Built with Docusaurus. `
+    }
   },
   presets: [
     [
@@ -71,12 +74,12 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           editUrl:
-            "https://github.com/dicedtomatoreal/zipline-docs/edit/master/",
+            "https://github.com/dicedtomatoreal/zipline-docs/edit/master/"
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-        },
-      },
-    ],
-  ],
+          customCss: require.resolve("./src/css/custom.css")
+        }
+      }
+    ]
+  ]
 };
