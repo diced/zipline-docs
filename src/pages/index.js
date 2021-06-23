56c21ed821f33826584bce0a7a39d497c96343c6
diced <pranaco2@gmail.com>
2021-06-22 20:49:54 -0700

    change desc

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,7 +57,7 @@ function Home() {
   return (
     <Layout
       title='Documentation'
-      description="Speed & reliability first.">
+      description="Fast & lightweight file uploading.">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
