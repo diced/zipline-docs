8f6b998ec6b8282580d9a24603a65e3d85fa0f95
BuildTools <unconfigured@null.spigotmc.org>
2021-08-26 23:55:53 +0200

    p

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
   github: 'https://github.com/shuding/nextra',
   docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  titleSuffix: ' – Zipline Docs',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Zipline</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        Fast & lightweight file uploading.
       </span>
     </>
   ),
@@ -16,52 +16,24 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-icon-180x180.png"
-      />
+      <meta name="description" content="Zipline: documentation" />
+      <meta name="og:description" content="Zipline: documentation" />
+      <meta name="twitter:site:domain" content="zipline.diced.me" />
+      <meta name="twitter:url" content="https://zipline.diced.me/" />
+      <meta name="og:title" content="Zipline: documentation" />
+      <meta name="apple-mobile-web-app-title" content="Zipline" />
       <link
         rel="icon"
         type="image/png"
-        sizes="192x192"
-        href="/android-icon-192x192.png"
+        sizes="256x256"
+        href="/favicon.ico"
       />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="96x96"
-        href="/favicon-96x96.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
     </>
   ),
-  search: true,
+  search: false,
   prevLinks: true,
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
-  unstable_faviconGlyph: '👋',
+  footerText: <>© {new Date().getFullYear()}  Zipline</>
 }
