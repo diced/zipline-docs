2492278ae12038ec947bce5cb8d48d678477e0cd
diced <pranaco2@gmail.com>
2024-12-31 02:22:13 -0800

    fix: types

--- src/lib/parser/bytes.ts
+++ src/lib/parser/bytes.ts
@@ -6,6 +6,6 @@ export function bytes(
   value: string | number,
   options?: BytesOptions,
 ): string | number {
-  if (typeof value === 'string') return bytesFn(value);
-  return bytesFn(Number(value), { ...options, unitSeparator: ' ' });
+  if (typeof value === 'string') return bytesFn(value) ?? 0;
+  return bytesFn(Number(value), { ...options, unitSeparator: ' ' }) ?? '';
 }
