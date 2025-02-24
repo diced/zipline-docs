b6b3234ccfa161bd759273bd148fbc0a5853eaeb
Jay <gogojayvin923@gmail.com>
2025-02-24 11:13:39 -0800

    A different kind of doc's version api. Takes either a commit or semver.

--- docs/api/version.md
+++ docs/api/version.md
@@ -2,27 +2,33 @@
 
 <Alert type="info">
   Requires [authentication](/docs/api#authentication)
+  
+  Requires [`WEBSITE_SHOW_VERSION`](/docs/config/website#website_show_version)
 </Alert>
 
 ## <APIBadge type="GET" /> Get version info
 
-### Headers
-
-Upload a file to Zipline, for more options see the [configuration](/docs/guides/upload-options) page.
-
-### Body (multipart/form-data)
-
-Field names of files should be `file`, if you want to upload multiple files then just append multiple fields with the name still being `file`.
-
 ### <APIBadge type="200" /> Ok (JSON)
 
-| Field Name | Type               | Description                    |
-| ---------- | ------------------ | ------------------------------ |
-| `files`    | `array of strings` | The URLs of the uploaded files |
+| Field Name         | Type                   | Description                                                                                              |
+| ------------------ | ---------------------- | -------------------------------------------------------------------------------------------------------- |
+| `isUpstream`       | `boolean`              | If the instance is on stable or upstream git                                                             |
+| `update`           | `boolean`              | If the instances needs an update                                                                         |
+| `updateToType`     | `stable` OR `upstream` | The instance's recommended update                                                                        |
+| `version.stable`   | `string`               | Latest stable version                                                                                    |
+| `version.upstream` | `string`               | Latest upstream version                                                                                  |
+| `version.current`  | `string`               | Instance's current version, either a commit hash or a semver. Commit hash is shortened on the dashboard. |
 
 ```json
 {
-  "files": ["https://example.com/asdfgh.png"]
+  "isUpstream": true,
+  "update": true,
+  "updateToType": "upstream",
+  "versions": {
+    "stable": "4.0.0",
+    "upstream": "b842d594f78e0c5b97c1ab83cf6224509a41c402",
+    "current": "3.7.13"
+  }
 }
 ```
 
--- src/pages/api/version.ts
+++ src/pages/api/version.ts
@@ -1,3 +1,4 @@
+import { Endpoints } from '@octokit/types';
 import type { NextRequest } from 'next/server';
 import { parse } from 'semver';
 
@@ -18,9 +19,28 @@ export const config = {
   runtime: 'edge',
 };
 
-async function getLatestRelease() {
+type Tags = Endpoints['GET /repos/{owner}/{repo}/tags']['response']['data'];
+type Commits =
+  Endpoints['GET /repos/{owner}/{repo}/commits']['response']['data'];
+
+async function getTags(): Promise<Tags> {
+  const res = await fetch('https://api.github.com/repos/diced/zipline/tags', {
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      ...(process.env.GITHUB_TOKEN
+        ? { Authorization: 'token ' + process.env.GITHUB_TOKEN }
+        : {}),
+    },
+  });
+
+  if (!res.ok) return [];
+
+  return await res.json();
+}
+
+async function getCommits(): Promise<Commits> {
   const res = await fetch(
-    'https://api.github.com/repos/diced/zipline/releases/latest',
+    'https://api.github.com/repos/diced/zipline/commits/v3',
     {
       headers: {
         Accept: 'application/vnd.github.v3+json',
@@ -31,32 +51,69 @@ async function getLatestRelease() {
     },
   );
 
-  return res.json();
-}
-
-async function getLatestUpstreamRelease() {
-  // read the package.json
-  const res = await fetch(
-    'https://raw.githubusercontent.com/diced/zipline/trunk/package.json',
+  const latestv3 = ((await res.json()) as Commits[0]).sha;
+  const realRes = await fetch(
+    `https://api.github.com/repos/diced/zipline/commits?sha=${latestv3}`,
+    {
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+        ...(process.env.GITHUB_TOKEN
+          ? { Authorization: 'token ' + process.env.GITHUB_TOKEN }
+          : {}),
+      },
+    },
   );
 
-  const { version } = await res.json();
+  return await realRes.json();
+}
 
-  return version;
+async function getCommit(c: string | null): Promise<Commits[0] | null> {
+  const res = await fetch(
+    `https://api.github.com/repos/diced/zipline/commits/${c}`,
+    {
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+        ...(process.env.GITHUB_TOKEN
+          ? { Authorization: 'token ' + process.env.GITHUB_TOKEN }
+          : {}),
+      },
+    },
+  );
+
+  if (!res.ok) return null;
+
+  return await res.json();
 }
 
 export default async function handler(req: NextRequest) {
   const { searchParams } = new URL(req.url);
 
-  const c = searchParams.get('c');
+  const com = searchParams.get('c');
 
-  const latestStable = await getLatestRelease();
+  const tags = await getTags();
+  const commits = await getCommits();
 
-  const stable = parse(latestStable.tag_name.replace('v', ''));
-  const upstream = parse(await getLatestUpstreamRelease());
-  const current = parse(c);
+  if (tags.length === 0 || commits.length === 0)
+    return new Response(
+      JSON.stringify({
+        error: 'no available version',
+      }),
+      {
+        status: 500,
+        headers: {
+          'content-type': 'application/json',
+        },
+      },
+    );
 
-  if (!current || !stable || !upstream) {
+  const stable = parse(tags[0].name.replace('v', '')),
+    upstream = commits[0],
+    instanceVer = tags.find(
+      (t) => t.name.replace('v', '') === com || t.commit.sha === com,
+    ),
+    instnceCom = await getCommit(com);
+
+  if (!stable || (!instnceCom && !instanceVer)) {
     return new Response(
       JSON.stringify({
         error: 'Invalid version',
@@ -70,27 +127,35 @@ export default async function handler(req: NextRequest) {
     );
   }
 
-  const isUpstream = current.prerelease.length !== 0;
+  const isUpstream =
+    tags.findIndex((t) => t.commit.sha === instnceCom?.sha) === -1;
 
   const response: VersionResponse = {
     git: {
       stable: stable.version,
-      upstream: upstream.version,
+      upstream: upstream.sha,
     },
     isUpstream,
-    current: current.version,
+    current:
+      tags
+        .find(
+          (t) =>
+            t.commit.sha === instnceCom?.sha || t.name.replace('v', '') === com,
+        )
+        ?.name.replace('v', '') || instnceCom?.sha,
   };
 
-  if (isUpstream) {
+  if (isUpstream)
     response.update = {
-      stable: stable.compare(current) > 0,
-      upstream: upstream.compare(current) > 0,
+      upstream:
+        commits.findIndex((c) => c.sha === instnceCom?.sha) === -1
+          ? true
+          : commits.findIndex((c) => c.sha === instnceCom?.sha) > 0,
     };
-  } else {
+  else
     response.update = {
-      stable: stable.compare(current) > 0,
+      stable: tags[0].commit.sha !== instanceVer?.commit.sha,
     };
-  }
 
   return new Response(JSON.stringify(response), {
     status: 200,
