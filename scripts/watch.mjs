d0d4b109143fbcf04e98883ae52fa587a2068126
diced <git@diced.sh>
2025-05-27 20:21:59 -0700

    feat: more api docs

--- scripts/watch.mjs
+++ scripts/watch.mjs
@@ -3,12 +3,10 @@ import { generator } from './generator.mjs';
 
 await generator('build');
 
-chokidar
-  .watch('docs', { ignoreInitial: true, ignored: (p) => p.includes('.mdx') })
-  .on('all', async () => {
-    try {
-      await generator('watch');
-    } catch (e) {
-      console.log('[watch] error: ' + e.message);
-    }
-  });
+chokidar.watch('docs', { ignoreInitial: true }).on('all', async () => {
+  try {
+    await generator('watch');
+  } catch (e) {
+    console.log('[watch] error: ' + e.message);
+  }
+});
